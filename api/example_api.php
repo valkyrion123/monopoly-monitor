a2373bc7aaab8f5b1e643450ecef85adaace4cf3
Amos Christian <amos.christian@hreasily.com>
2016-12-19 16:22:51 +0700

    Add starter file structure

---
+++ api/example_api.php
@@ -0,0 +1,12 @@
+<?php 
+
+//DO NOT DELETE THIS FILE
+//put all the Ajax API here
+
+//For convention file naming use {description}_api.php
+
+function example($a, $b){
+	return $a + $b;
+}
+
+?>
---
+++ js/main.js
@@ -0,0 +1,3 @@
+$(document).ready(function(){
+    //put javascript function here
+});
