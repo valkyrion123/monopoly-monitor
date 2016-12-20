10c11dc0ec32330524f4e7a9f8a8774ae416ef37
afadhilen <fadhil.kreydle@gmail.com>
2016-12-20 22:59:45 +0700

    Include view page

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+	<title>Monopoly E-Banking</title>
+	<link rel="stylesheet" href="themes/monopoly-monitor.css" />
+	<link rel="stylesheet" href="themes/jquery.mobile.icons.min.css" />
+	<link rel="stylesheet" href="framework/jquery.mobile-1.4.5.css" />
+	<script src="framework/jquery-3.1.1.min.js"></script>
+	<script src="framework/jquery-1.11.1.min.js"></script>
+	<script src="framework/jquery.mobile-1.4.5.js"></script>
+	<script src="js/main.js"></script>
+</head>
+<body>
+
+	<?php include("view/main-menu.php"); ?>   
+	<?php include("view/player.php"); ?>   
+	<?php include("view/transfer.php"); ?>   
+	<?php include("view/bank.php"); ?>   
+
+
+</body>
+</html>
