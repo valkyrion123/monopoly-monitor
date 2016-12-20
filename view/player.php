0e2e2863f215a92d3d941b3b5d44c2f0b0622006
Amos Christian <amos.christian@hreasily.com>
2016-12-20 23:12:09 +0700

    Change navigation button

--- view/player.php
+++ view/player.php
@@ -1,20 +1,19 @@
-	<div data-role='page' id='player' data-theme='b'>
-		<div data-role='header' data-position='fixed'>
-			<h2>Monopoly</h2>
-			<a href="#main-menu" data-icon='home' data-role='button' data-transition='slide'>Home</a>
-		</div>		
+<div data-role='page' id='player' data-theme='b'>
+	<div data-role='header' data-position='fixed'>
+		<h2>Monopoly</h2>
+		<a href="#main-menu" data-icon='home' data-role='button' data-transition='slide'>Home</a>
+	</div>		
 
-		<div data-role='content' class='ui-content'>
-			<h2 align='center'>Player</h2>
-		</div>
-
-		<div data-role='navbar' data-iconpos="left">
-			<a href="#bank" data-icon='tag' data-role='button' data-transition='slide'>Bank</a>
-			<a href="#player" data-icon='user' data-role='button' data-transition='slide'>Player</a>
-			<a href="#transfer" data-icon='minus' data-role='button' data-transition='slide'>Transfer</a>
-		</div>
-
-		<div data-role='footer' data-position="fixed">
-			<h1>&copy; Stupid Inhooman 2016</h1>
-		</div>
+	<div data-role='content' class='ui-content'>
+		<h2 align='center'>Player</h2>
 	</div>
+
+	<div data-role='navbar' data-iconpos="left">
+		<a href="#player-list" data-icon='tag' data-role='button' data-transition='slide'>List</a>
+		<a href="#leaderboard" data-icon='minus' data-role='button' data-transition='slide'>Leaderboard</a>
+	</div>
+
+	<div data-role='footer' data-position="fixed">
+		<h1>&copy; Stupid Inhooman 2016</h1>
+	</div>
+</div>
